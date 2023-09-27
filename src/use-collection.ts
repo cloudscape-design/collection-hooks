b9adcd0b17514d5cc456a85c4ab35f597d0b2242
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-09-27 10:17:13 +0200

    update usecollection types

--- src/use-collection.ts
+++ src/use-collection.ts
@@ -2,36 +2,39 @@
 // SPDX-License-Identifier: Apache-2.0
 import { useRef } from 'react';
 import { processItems, processSelectedItems, itemsAreEqual } from './operations/index.js';
-import { UseCollectionOptions, UseCollectionResult, CollectionRef } from './interfaces';
+import { UseCollectionOptions, UseCollectionResult, CollectionRef, PropertyFilterOperator } from './interfaces';
 import { createSyncProps } from './utils.js';
 import { useCollectionState } from './use-collection-state.js';
 
-export function useCollection<T>(allItems: ReadonlyArray<T>, options: UseCollectionOptions<T>): UseCollectionResult<T> {
+export function useCollection<T, OperatorType extends string = PropertyFilterOperator>(
+  allItems: ReadonlyArray<T>,
+  options: UseCollectionOptions<T, OperatorType>
+): UseCollectionResult<T, OperatorType> {
   const collectionRef = useRef<CollectionRef>(null);
-  const [state, actions] = useCollectionState(options, collectionRef);
+  const [state, actions] = useCollectionState(options as any, collectionRef);
   const { items, allPageItems, pagesCount, filteredItemsCount, actualPageIndex } = processItems(
     allItems,
     state,
-    options
+    options as any
   );
   if (options.selection && !options.selection.keepSelection) {
-    const newSelectedItems = processSelectedItems(items, state.selectedItems, options.selection.trackBy);
-    if (!itemsAreEqual(newSelectedItems, state.selectedItems, options.selection.trackBy)) {
+    const newSelectedItems = processSelectedItems(items as any, state.selectedItems as any, options.selection.trackBy);
+    if (!itemsAreEqual(newSelectedItems, state.selectedItems as any, options.selection.trackBy)) {
       // This is a recommended pattern for how to handle the state, dependent on the incoming props
       // https://reactjs.org/docs/hooks-faq.html#how-do-i-implement-getderivedstatefromprops
       actions.setSelectedItems(newSelectedItems);
     }
   }
   return {
-    items,
-    allPageItems,
+    items: items as any,
+    allPageItems: allPageItems as any,
     filteredItemsCount,
-    actions,
-    ...createSyncProps(options, state, actions, collectionRef, {
+    actions: actions as any,
+    ...createSyncProps(options as any, state, actions, collectionRef, {
       actualPageIndex,
       pagesCount,
       allItems,
       allPageItems,
     }),
-  };
+  } as any;
 }
