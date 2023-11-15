110dc1a522451bfd8994c3d3933af974a42e6bfb
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-11-15 09:37:15 +0100

    add extra unit test

--- src/__tests__/operations/combined.test.ts
+++ src/__tests__/operations/combined.test.ts
@@ -95,3 +95,30 @@ test('all together', () => {
   expect(filteredItemsCount).toEqual(8);
   expect(processed).toEqual([items[3], items[4], items[1]]);
 });
+
+test('filtering with property filtering', () => {
+  const items = [
+    { id: 1, value: 'a' },
+    { id: 2, value: 'ab' },
+    { id: 3, value: 'abc' },
+    { id: 4, value: 'abcd' },
+    { id: 5, value: 'bcd' },
+    { id: 6, value: 'cd' },
+    { id: 7, value: 'd' },
+  ];
+  const { items: processed, filteredItemsCount } = processItems(
+    items,
+    {
+      filteringText: 'ab',
+      propertyFilteringQuery: { operation: 'and', tokens: [{ propertyKey: 'value', operator: ':', value: 'cd' }] },
+    },
+    {
+      filtering: {},
+      propertyFiltering: {
+        filteringProperties: [{ key: 'value', operators: [':'], propertyLabel: '', groupValuesLabel: '' }],
+      },
+    }
+  );
+  expect(filteredItemsCount).toEqual(1);
+  expect(processed).toEqual([items[3]]);
+});
