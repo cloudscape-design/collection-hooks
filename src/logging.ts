d93c886401679bfc0dd37fc5180cfaa0aa79a671
Boris Serdiuk <serdiuk@amazon.com>
2024-12-26 23:33:25 +0000

    fix: Upgrade typescript configuration

--- src/logging.ts
+++ src/logging.ts
@@ -1,11 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-/**
- * Allow use of `process.env.NODE_ENV` specifically.
- */
-declare const process: { env: { NODE_ENV?: string } };
-
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const messageCache = new Set<string>();
