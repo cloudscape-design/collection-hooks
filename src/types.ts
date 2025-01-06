d6a57e3303ab9ca5433f93613985105d85525ac0
Boris Serdiuk <serdiuk@amazon.com>
2025-01-06 11:53:59 +0100

    fix: Mark package as side effects free

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@
 
 /**
  * subset of node.js types, safe to use in browser and bundlers
+ * we do not use `lib.dom` types because they are not available in SSR environment
  */
 declare const process: { env: { NODE_ENV?: string } };
 declare const console: { warn: (...args: Array<any>) => void };
